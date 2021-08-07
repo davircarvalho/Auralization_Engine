d547d6969894ae9e7a6a96989f4d9f36dd61ddc8
Elephant ~ <r.davicarvalho@gmail.com>
2021-08-07 18:30:22 -0300

    New visual + added top view + new distance variation method and a lot more

--- src/AirAbsorption/test_airAbs.m
+++ src/AirAbsorption/test_airAbs.m
@@ -2,20 +2,20 @@ clc; clear all;
 % test distnce variation influencce in air absorption coef
 addpath('D:\Documentos\1 - Work\Individualized_HRTF_Synthesis\Functions')
 
-N = 200;
+N = 200; % number of samples 
 fs = 44100;
-f = linspace(0, fs-fs/N,N)';
+freq = linspace(0, fs-fs/N,N)';
 for k = 1:N
-    [~, alpha_iso(k,1), ~, ~]=air_absorption(f(k));
+    [~, alpha_iso(k,1), ~, ~]=air_absorption(freq(k));
 end
-dist= 5;
+dist= 15;
 alpha = (dist*alpha_iso);
 alpha(:,2) = alpha(:,1);
 hFigure =figure();
-plot(f(1:N/2), alpha(1:N/2,1), 'r', 'linewidth', 2)
+plot(freq(1:N/2), alpha(1:N/2,1), 'r', 'linewidth', 2)
 xlabel('Frequência (Hz)')
-ylabel('Atenuação (dB/m)')
-xlim([0 f(N/4)])
+ylabel('Atenuação (dB)')
+xlim([0 freq(N/4)])
 grid on
 set(gca, 'fontsize', 13)
 % arruma_fig('% 4.0f','% 2.1f','virgula')
@@ -23,28 +23,37 @@ set(gca, 'fontsize', 13)
 filename = [pwd, '/ISOairabs.pdf' ];
 % exportgraphics(hFigure,filename,'BackgroundColor','none','ContentType','vector')
 
-%% Aplicar em HRTF 
+%% Carregar HRTF 
 Obj = SOFAload('..\individuo_141.sofa');
 [itd, Obj] = SOFAgetITD(Obj);
 ir = shiftdim(Obj.Data.IR, 2);
-IR = [ir(:,1,1), ir(:,1,2)];
 
-hrtf = 20*log10(abs(fft(IR)));
-y = hrtf - alpha;
+%% Generate filters 
 
-% figure()
-% plot(f, hrtf); hold on
-% plot(f, y)
-% legend('Original', 'Corrigido', 'location', 'best')
-% title(['Correção da absorção do ar para fonte a ' num2str(dist) 'm'])
-    
-% plot(y(1:N/2,:))
-% y_min = (get_min_phase(y(1:N/2,:), 'log', 'nonsymmetric'));
-% fymin = db(abs((y_min)));
-% plot( real(ifft(y_min)));
+% N — Filter order for FIR filters and the numerator and denominator orders for IIR filters.
+% F — Frequency vector. Frequency values in specified in F indicate locations where you provide specific filter response amplitudes.
+% A — Amplitude vector. Values in A define the filter amplitude at frequency points you specify in f, the frequency vecto
+tic
+order = 50;
+f = freq(1:N/2)./max(freq(1:N/2));
+d = fdesign.arbmag('N,F,A',order, f , 10.^(-alpha(1:N/2,1)./20));
+Hd = design(d,'freqsamp','SystemObject',true);
+toc
+% fvtool(Hd,'MagnitudeDisplay','Zero-phase','Color','White');
+x = ir(:,1,1);
+out = Hd(x);
 
-% xlim([0, f(N/2)])
 
+%% plot
+xf = db(abs(fft(x)));
+outf = db(abs(fft(out)));
+N = length(x);
+fqq = linspace(0, fs-fs/N, N);
+
+
+plot(fqq, xf);hold on 
+plot(fqq, outf);
+legend(['original'; 'filtered'])
 
 %%
 function y = fast_conv(x1, x2)
