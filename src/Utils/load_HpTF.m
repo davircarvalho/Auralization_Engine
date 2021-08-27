cb1665565ac079cf6b11c5b43367f2956ebc0f84
Elephant ~ <r.davicarvalho@gmail.com>
2021-08-27 16:08:50 -0300

    minor fix

--- src/Utils/load_HpTF.m
+++ src/Utils/load_HpTF.m
@@ -6,7 +6,7 @@ function [HpIR, H_out, freq] = load_HpTF(filepath, fs)
 %           fs:       amostragem do filtro na saida
 % SAIDAS:
 %           HpIR:     Headphone imppulse response (filtro de correcao no tempo)
-%           H:        Resposta do fone
+%           H_out:    Resposta do fone
 %           freq:     Vetor de frequencias para o filtro 
 
 %% Config
@@ -36,23 +36,42 @@ freq_mag_raw = clean_file(idx_freq == false).';
 
 % Inverter magnitude e janelar na frequencia 
 N_win = length(freq_mag_raw);
-win = tukeywin(N_win, 0.03  );
+win = tukeywin(N_win, 0.03);
 % win(round(N_win/2):end) = 1;
 inv_freq_mag_raw = (1./(10.^(freq_mag_raw./20))) .* win;
 
 % Gerar vetor no tempo
 % remover interpolação para frequencias alem das fornecidas (acima e abaixo)
-idx_low = dsearchn(freq.', freq_vec_raw(1));
-idx_up = dsearchn(freq.', freq_vec_raw(end));
+[~, idx_low] = min(abs(freq - freq_vec_raw(1)));
+[~, idx_high] = min(abs(freq - freq_vec_raw(end)));
 
-freq(1:idx_low) = [];
-freq(idx_up:end) = [];
+N_up = 2*length(freq(1:idx_low));
+N_down = 2*length(freq(idx_high:end));
+win_up = hann(N_up);
+win_up = win_up(1:N_up/2);
+
+win_down = hann(N_down);
+win_down = win_down((N_down/2+1):end);
+
 
 % Criar amostragem de acordo com a taxa de amostragem desejada 
-H = interp1(freq_vec_raw, inv_freq_mag_raw,  freq, 'makima');
-H_out = interp1(freq_vec_raw, 10.^(freq_mag_raw./20),  freq, 'makima');
+H = interp1(freq_vec_raw, inv_freq_mag_raw,  freq, 'makima', 'extrap');
+H_out = interp1(freq_vec_raw, 10.^(freq_mag_raw./20),  freq, 'makima', 'extrap');
+
+H(1:idx_low) = 10.^((20*log10(H(1:idx_low)).*win_up')./20);
+H(idx_high:end) = 10.^((20*log10(H(idx_high:end)).*win_down')./20);
 
 
 Hmin = get_min_phase(H.', 'linear', 'nonsymmetric');
 HpIR = real(ifft(Hmin));
+
+
+% Cortar RI para tamanho que contenha ate a menor fequencia conhecida
+min_sz = ceil(fs/freq_vec_raw(1));
+if mod(min_sz, 2) ~= 0
+    min_sz = min_sz+1;
+end
+
+HpIR(4*min_sz+1:end) = [];
+
 end
